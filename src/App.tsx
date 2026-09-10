bfc55896ad58f5c40b63959f88050c00b935673e
ayallau <ayallau@gmail.com>
2026-09-10 22:58:00 +0300

    feat: add initial AppShell

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import './App.css'
 
 function App() {
-  return <h1>TaskFlow</h1>;
+  return (
+    <div>
+      <header>
+        <h1>TaskFlow</h1>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h2>All My Tasks</h2>
+        <section>
+
+        </section>
+      </main>
+      <footer>
+        <p>Copyright © 2026 Ayal Laufer</p>
+      </footer>
+    </div>
+  );
 }
 
 export default App
